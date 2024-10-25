c798be5774b51d99465f9dff7ba1e5040ac81cd5
Saliram Chaudhary <saliramchaudhary1@gmail.com>
2024-10-25 15:06:05 +0530

    added new program

--- Python/Hello.py
+++ Python/Hello.py
@@ -13,35 +13,35 @@
 # str = "Saliram"
 # print(str.split()
 
-#type Check
+# TYPE CHECK
 # a = 505
 # print(type (a))
-# Type Casting
+# TYPE CASTING
 
 # a = int (input("Enter number a : "))
 # b = int (input("Enter number b : "))
 # c = a + b
 # print("Sum is : ", c)
 
-# Sum of thwo number with type casting
+# EXAMPLE OF TYPE CASTING 
 # a,b = map(int, input("Enter two number : ").split(','))
 # print(a+b)
 
 
-# Seperte value
+# VALUE SEPERATION
 # a = 10
 # b = 20
 # print('Value is ', a , b , sep=':', end='.....')
 
 
-# formate string
+# FORMAT STRING
 
 # a = 5
 # b = 10
 # c = a + b
 # print("Sum of number %i and %i is : %i" %(a,b,c))
 
-# String format
+# STRING FORMAT
 # name = "Saliram Chaudhary"
 # age = 22
 # gender = "male"
@@ -51,26 +51,59 @@
 # x = 15//2
 # print(x) output : 7
 
-# Program of find greater number
-a = 10
-b = 30
-c = 20
+# CHECK GREATER NUMBER 
+# a = 10
+# b = 30
+# c = 20
 
-if a > b and a > c :
-    greatest = a
-elif b > a and b > c :
-    greatest = b
-else :
-    greatest = c
+# if (a > b and a > c) :
+#     greatest = a
+# elif (b > a and b > c) :
+#     greatest = b
+# else :
+#     greatest = c
 
-print("The greatest number is :",greatest)
+# print("The greatest number is :",greatest)
 
-# Program of find smallest number
-if a < b and a < c :
-    smallest = a
-elif b < a and b < c :
-    smallest = b
-else : 
-    smallest = c
+# CHECK SMALLEST NUMBER 
+# if (a < b and a < c) :
+#     smallest = a
+# elif (b < a and b < c) :
+#     smallest = b
+# else : 
+#     smallest = c
 
-print("Smallest number is :",smallest)
+# print("Smallest number is :",smallest)
+
+# BOOLEAN
+# print('Hello') if( 55 > 10) else print('Bye')
+
+# WAP TO ENTER A NUMBER IS POSITIVE OR NEGATIVE
+# a = int(input("Enter a number : "))
+# print("Number is positive") if(a >= 0) else print("Number is negative")
+
+# WAP TO ENTER A NUMBER IS EVEN OR ODD
+# n = int(input("Enter a number : ")) 
+# print("Even") if(n % 2 == 0) else print("Odd")
+
+
+# ARITHMETIC ASSIGN OPERATOR
+# a = 50
+# a += 10 # 60
+# a -= 10 # 40
+# print(a) 
+
+# MEMBERSHIP OPERATOR 
+# s = "saliram chy"
+# print('s' in s) #output : true
+# print('z' in s) #output : false
+# print('chy' in s) #output : true
+
+# email = "saliramchaudhary1@gmail.com"
+# print('Valid email') if('saliramchaudhary1@gmail.com' in email) else print('Invalid email')
+# print('Invalid email') if('email' not in email) else print('Valid email')
+
+# ARRAY SEARCH CHECK USING MEMBERSHIP OPERATOR      
+a = [10,20,30,40,50]
+s = int(input("Enter a number : "))
+print("Valid Search") if(s in a) else print("Invalid Search")
