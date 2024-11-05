414ea0cb39071eea6a256ca273452e6f369cc82e
Saliram Chaudhary <saliramchaudhary1@gmail.com>
2024-11-05 22:48:21 +0530

    add new language

---
+++ Node Js/Intro/intro.js
@@ -0,0 +1,7 @@
+const node = {
+  definition : 'Node.js is an open-source, cross-platform runtime environment for executing JavaScript    code outside of a browser.',
+  developer : 'Ryan Dahl',
+  release_date : ' 27 May 2009',
+  made : 'v8 is written in c++',
+}
+console.log(introd.node);
