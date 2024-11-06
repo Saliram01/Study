e907879ec939bf0acc7338f251589de8d49b2188
Saliram Chaudhary <saliramchaudhary1@gmail.com>
2024-11-07 02:22:41 +0530

    update files

---
+++ React Js/Vite_React/Product/src/components/CardList.jsx
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from "react";
+import Card from "./Card";
+import Input from "./Input";
+
+function CardList() {
+  const [card, setCard] = useState([]);
+  const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    fetch("https://fakestoreapi.in/api/products")
+      .then((res) => res.json())
+      .then((data) => {
+        setCard(data.products);
+      });
+  }, []);
+
+  return (
+    <>
+      <Input setQuery={setSearch} />
+      <div className="card-container">
+        {card
+          .filter((itm) => {
+            return itm.brand.includes(search);
+          })
+          .map((items) => (
+            <Card
+              key={items.id}
+              imageUrl={items.image}
+              title={items.title}
+              brand={items.brand}
+              price={items.price}
+            />
+          ))}
+      </div>
+    </>
+  );
+}
+
+export default CardList;
