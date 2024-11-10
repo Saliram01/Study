24b3cec0a7a23efabcbf7b8f970a0df6179d6d95
Saliram Chaudhary <saliramchaudhary1@gmail.com>
2024-11-11 02:13:33 +0530

    added Clock project

---
+++ React Js/V_Project_B/BharatClock/src/components/ClockHeading.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+function ClockHeading() {
+    return <h1 className='text-center py-3 fs-1 fw-bold text-primary font-monospace'>Bharat Clock</h1>
+}
+
+export default ClockHeading;
+
