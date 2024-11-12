dae090ee8371804e4f4712e261f0f2a68a84a24a
Saliram Chaudhary <saliramchaudhary1@gmail.com>
2024-11-12 15:09:47 +0530

    update files

--- JavaScript/Project/Table/src/app.js
+++ JavaScript/Project/Table/src/app.js
@@ -5,29 +5,38 @@ const box = document.querySelector('#container');
 const multiple = document.querySelector('#btn');
 
 
-single.addEventListener('click',() => {
-    let u = document.querySelector('.single').value;
-    for(let j = 1; j <= 10; j++) {
-        const container = document.createElement('div');
-        const p = document.createElement('p');
-        boxSingle.classList.add('border-2');
-        p.classList.add('mb-2')
-        p.innerText = `Table of : ${j}`;
-        container.classList.add('flex');
-        container.classList.add('justify-between');
+single.addEventListener('click', () => {
+    let u = document.querySelector('.singleOne').value;
+    const container = document.createElement('div');
+    const p = document.createElement('p');
+    p.innerText = `Table of : ${u}`;
+    container.classList.add('flex');
+    container.classList.add('border-2');
+    container.classList.add('justify-between');
+    container.classList.add('mb-2');
 
-        const operand = document.createElement('div');
-        operand.classList.add('px-2');
-        const ope = document.createElement('div');
-        ope.classList.add('px-2');
-        const count = document.createElement('div');
-        count.classList.add('px-2');
-        const equal = document.createElement('div');
-        equal.classList.add('px-2'); 
-        const total = document.createElement('div');
-        total.classList.add('px-2');
+    const operand = document.createElement('div');
+    operand.classList.add('p-2');
+    operand.classList.add('text-xl');
 
-        container.append(operand,ope,count,equal,total);
+    const ope = document.createElement('div');
+    ope.classList.add('p-2');
+    ope.classList.add('text-xl');
+
+    const count = document.createElement('div');
+    count.classList.add('p-2');
+    count.classList.add('text-xl');
+
+    const equal = document.createElement('div');
+    equal.classList.add('p-2');
+    equal.classList.add('text-xl');
+
+    const total = document.createElement('div');
+    total.classList.add('p-2');
+    total.classList.add('text-xl');
+
+    container.append(operand, ope, count, equal, total);
+    for (let j = 1; j <= 10; j++) {
 
         let div1 = document.createElement('div');
         div1.innerText = u;
@@ -49,16 +58,17 @@ single.addEventListener('click',() => {
         count.append(div3);
         equal.append(div4);
         total.append(div5);
-        boxSingle.append(container);
-        document.querySelector('.single').value = '';
     }
+    box.append(p);
+    boxSingle.append(container);
+    document.querySelector('.singleOne').value = '';
 })
 
 
 // MULTIPLE TABLE
-multiple.addEventListener('click',() => {
+multiple.addEventListener('click', () => {
     let user = document.querySelector('.multiple').value;
-    for(let i = 1; i <= user; i++) {
+    for (let i = 1; i <= user; i++) {
         const container = document.createElement('div');
         const p = document.createElement('p');
         p.classList.add('mb-2')
@@ -67,20 +77,30 @@ multiple.addEventListener('click',() => {
         container.classList.add('border-2');
         container.classList.add('justify-between');
         container.classList.add('mb-4');
+        
         const operand = document.createElement('div');
         operand.classList.add('p-2');
+         operand.classList.add('text-xl');  
+
         const ope = document.createElement('div');
         ope.classList.add('p-2');
+        ope.classList.add('text-xl');
+
         const count = document.createElement('div');
         count.classList.add('p-2');
+        count.classList.add('text-xl');
+
         const equal = document.createElement('div');
-        equal.classList.add('p-2'); 
+        equal.classList.add('p-2');
+        equal.classList.add('text-xl');
+
         const total = document.createElement('div');
         total.classList.add('p-2');
+        total.classList.add('text-xl');
 
-        container.append(operand,ope,count,equal,total);
-        
-        for(let j = 1; j <= 10; j++) {
+        container.append(operand, ope, count, equal, total);
+
+        for (let j = 1; j <= 10; j++) {
 
             let div1 = document.createElement('div');
             div1.innerText = i;
