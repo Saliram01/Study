ddd17d640546b782cd1e91ef73200c9ed72931fe
Saliram Chaudhary <saliramchaudhary1@gmail.com>
2024-11-14 23:03:14 +0530

    update react files

--- React Js/Vite_React/B_Project/TodoListReducerContext/src/components/Message.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Message() {
-  return (
-    <div className='message'>Enjoy Today Is Holiday</div>
-  )
-}
-
-export default Message
--- React Js/Vite_React/T_Project/05-CustomContextMenu/src/hooks/useFilter.jsx
+++
@@ -1,9 +0,0 @@
-import React, { useState } from 'react'
-
-export function useFilter(dataList, callBack){
-    const [query, setQuery] = useState('');
- 
-    const filteredData = dataList.filter((data) => callBack(data).toLowerCase().includes(query));
-
-    return [filteredData, setQuery];
-}
