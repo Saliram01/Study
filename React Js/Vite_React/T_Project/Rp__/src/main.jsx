87bc62c07203c8f86464b47819e12f747de60df3
Saliram Chaudhary <saliramchaudhary1@gmail.com>
2024-11-17 00:56:26 +0530

    update files

--- React Js/Vite_React/T_Project/Rp__/src/main.jsx
+++ React Js/Vite_React/T_Project/Rp__/src/main.jsx
@@ -28,117 +28,135 @@ import ImageGallery from "./components/p_list/ImageGallery";
 import TodoContainer from "./components/p_list/TodoList/TodoContainer";
 
 // Plain Obj Routes
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    children: [
-      {
-        path: "/",
-        element: <Home />,
-      },
-      {
-        path: "*",
-        element: <Error404 />,
-      },
-      {
-        path: "about",
-        element: <About />,
-      },
-      {
-        path: "blog",
-        element: <Blog />,
-      },
-      {
-        path: "project",
-        element: <Project />,
-      },
-      {
-        path: "contact",
-        element: <Contact />,
-      },
-      {
-        path: "blog/:id",
-        element: <BlogDetail />,
-      },
-      {
-        path: "useState",
-        element: <UseState />,
-      },
-      {
-        path: "useEffect",
-        element: <UseEffect />,
-      },
-      {
-        path: "useContext",
-        element: <Context />,
-      },
-      {
-        path: "useRef",
-        element: <UseRef />,
-      },
-      {
-        path: "useMemo",
-        element: <UseMemo />,
-      },
-      {
-        path: "useCallback",
-        element: <UseCallback />,
-      },
-      {
-        path: "useReducer",
-        element: <UseReducer />,
-      },
-      {
-        path: "useLayoutEffect",
-        element: <UseLayoutEffect />,
-      },
-      {
-        path: "customHook",
-        element: <UseLocalStorage />,
-      },
-      {
-        path: "bgchanger",
-        element: <BgChanger />,
-      },
-      {
-        path: "enquery",
-        element: <EnqueryForm />,
-      },
-      {
-        path: "sign-up",
-        element: <SignUpForm />,
-      },
-      {
-        path: "meme",
-        element: <Meme />,
-      },
-      {
-        path: "image",
-        element: <ImageGallery />,
-      },
-      {
-        path: "todoList",
-        element: <TodoContainer />,
-      },
-    ],
-  },
-]);
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <App />,
+//     children: [
+//       {
+//         path: "/",
+//         element: <Home />,
+//       },
+//       {
+//         path: "*",
+//         element: <Error404 />,
+//       },
+//       {
+//         path: "about",
+//         element: <About />,
+//       },
+//       {
+//         path: "blog",
+//         element: <Blog />,
+//       },
+//       {
+//         path: "project",
+//         element: <Project />,
+//       },
+//       {
+//         path: "contact",
+//         element: <Contact />,
+//       },
+//       {
+//         path: "blog/:id",
+//         element: <BlogDetail />,
+//       },
+//       {
+//         path: "useState",
+//         element: <UseState />,
+//       },
+//       {
+//         path: "useEffect",
+//         element: <UseEffect />,
+//       },
+//       {
+//         path: "useContext",
+//         element: <Context />,
+//       },
+//       {
+//         path: "useRef",
+//         element: <UseRef />,
+//       },
+//       {
+//         path: "useMemo",
+//         element: <UseMemo />,
+//       },
+//       {
+//         path: "useCallback",
+//         element: <UseCallback />,
+//       },
+//       {
+//         path: "useReducer",
+//         element: <UseReducer />,
+//       },
+//       {
+//         path: "useLayoutEffect",
+//         element: <UseLayoutEffect />,
+//       },
+//       {
+//         path: "customHook",
+//         element: <UseLocalStorage />,
+//       },
+//       {
+//         path: "bgchanger",
+//         element: <BgChanger />,
+//       },
+//       {
+//         path: "enquery",
+//         element: <EnqueryForm />,
+//       },
+//       {
+//         path: "sign-up",
+//         element: <SignUpForm />,
+//       },
+//       {
+//         path: "meme",
+//         element: <Meme />,
+//       },
+//       {
+//         path: "image",
+//         element: <ImageGallery />,
+//       },
+//       {
+//         path: "todoList",
+//         element: <TodoContainer />,
+//       },
+//     ],
+//   },
+// ]);
 
 // Nested routes with jsx
-// const routing = createBrowserRouter(
-//   createRoutesFromElements(
-//     <Route path="/" element={<App />}>
-//       <Route path="/" element={<Home/>}/>
-//       <Route path="/about" element={<About/>}/>
-//       <Route path="/blog" element={<Blog/>}/>
-//       <Route path="/project" element={<Project/>}/>
-//     </Route>
-//   )
-// );
+const routing = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route index element={<Home/>}/>
+      <Route path="about" element={<About/>}/>
+      <Route path="blog" element={<Blog/>}/>
+      <Route path="contact" element={<Contact/>}/>
+      <Route path="project" element={<Project/>}/>
+      <Route path="useContext" element={<Context/>}/>
+      <Route path="*" element={<Error404/>}/>
+      <Route path="blog/:id" element={<BlogDetail/>}/>
+      <Route path="useEffect" element={<UseEffect/>}/>
+      <Route path="useRef" element={<UseRef/>}/>
+      <Route path="useState" element={<UseState/>}/>
+      <Route path="useMemo" element={<UseMemo/>}/>
+      <Route path="useCallback" element={<UseCallback/>}/>
+      <Route path="useReducer" element={<UseReducer/>}/>
+      <Route path="useLayoutEffect" element={<UseLayoutEffect/>}/>
+      <Route path="customHook" element={<UseLocalStorage/>}/>
+      <Route path="bgchanger" element={<BgChanger/>}/>
+      <Route path="enquery" element={<EnqueryForm/>}/>
+      <Route path="sign-up" element={<SignUpForm/>}/>
+      <Route path="meme" element={<Meme/>}/>
+      <Route path="image" element={<ImageGallery/>}/>
+      <Route path="todoList" element={<TodoContainer/>}/>
+    </Route>
+  )
+);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <RouterProvider router={routing} />
   </StrictMode>
 );
