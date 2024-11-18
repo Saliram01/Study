9714a922ca64837953faf7aaa7ea9f8215e90d6d
Saliram Chaudhary <saliramchaudhary1@gmail.com>
2024-11-19 01:24:50 +0530

    added conditional statement files of java

---
+++ JavaCore/src/conditional_statement/If_Else.java
@@ -0,0 +1,17 @@
+package conditional_statement;
+
+public class If_Else {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int num = 4;
+		
+		if(num % 2 == 0) {
+			System.out.println("Number is true");
+		} else {
+			System.out.println("Number is false");
+		}
+	}
+
+}
---
+++ JavaCore/src/conditional_statement/Leader_If_Else.java
@@ -0,0 +1,18 @@
+package conditional_statement;
+
+public class Leader_If_Else {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		boolean isSenior = false;
+		boolean isAdult = false;
+		
+		if(isSenior) { System.out.println("Hello Senior");}
+		
+		else if(isAdult) { System.out.println("Hello Adult");}
+		
+		else {System.out.println("Hello Child");}
+	}
+
+}
