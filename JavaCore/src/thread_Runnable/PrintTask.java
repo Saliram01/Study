9dc2b7f8791ac524e593fdb3e30c02837b4680e0
Saliram Chaudhary <saliramchaudhary1@gmail.com>
2024-11-25 22:29:27 +0530

    added runnable file of java

--- JavaCore/src/thread_Runnable/PrintTask.java
+++ JavaCore/src/thread_Runnable/PrintTask.java
@@ -4,7 +4,13 @@ public class PrintTask implements Runnable {
 	
 	public void run() {
 		// Print Task
-		for (int i = 1; i < 10; i++) {
+		for (int i = 1; i < 100; i++) {
+			try {
+				Thread.sleep(10);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 			System.out.printf(" %d %c ", i, targetChar);
 		}
 		System.out.printf(" \n %s * task is complete \n", 
