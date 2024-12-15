8613e0c508d170f5f0c8e259348b5c4351b97105
Saliram Chaudhary <saliramchaudhary1@gmail.com>
2024-12-15 15:02:38 +0530

    update files

--- React Js/Vite/B_Project/Todo_List/TodoListReducerContext/src/components/TodoInput.jsx
+++ React Js/Vite/B_Project/Todo_List/TodoListReducerContext/src/components/TodoInput.jsx
@@ -1,25 +1,26 @@
 import React, { useContext, useRef } from "react";
-import {TodoListStore} from '../store/TodoItemListStore'
+import { TodoListStore } from "../store/TodoItemListStore";
 
 function TodoInput() {
-  const {addNewItems} = useContext(TodoListStore);
+  const { addNewItems } = useContext(TodoListStore);
   const refElementVal = useRef();
-  
+
   function handleOnClick(e) {
     e.preventDefault();
-    const refval = refElementVal.current.value;
-    refElementVal.current.value = '';
-    addNewItems(refval);
+
+    if (refElementVal.current.value === "") {
+      return;
+    } else {
+      const refval = refElementVal.current.value;
+      refElementVal.current.value = "";
+      addNewItems(refval);
+    }
   }
 
   return (
-      <form onSubmit={handleOnClick} className="input-container">
-      <input
-        type="text"
-        placeholder="Enter your todo..."
-        ref={refElementVal}
-      />
-      <button >Add</button>
+    <form onSubmit={handleOnClick} className="input-container">
+      <input type="text" placeholder="Enter your todo..." ref={refElementVal} />
+      <button>Add</button>
     </form>
   );
 }
